0fa8b2de393cfe73bf97464508dd7ccd7174104e
tobiarnold <77453801+tobiarnold@users.noreply.github.com>
2022-10-18 17:03:46 +0200

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def main():
                   - Die folgende Anwendung liefert eine **Prognose des Mietpreises** (warm) für ein Wohnobjekt anhand der Eingaben des Nutzers.
                   - Unsere Datenbank entält über **178.000 Wohnobjekte** und wird stetig erweitert.
                   - Neben der Prognose können auch verschiedene **Diagramme** und eine **interaktive Karte** aus dem Datensatz betrachtet werden.
+                  ⚠️ bei Zugriff mit mobilen Geräten ist der Filter für die Prognose standardmäßig ausgeblendet und lässt sich mit dem Pfeil oben links wieder einblenden
                    """)
     st.info("Für Premiumuser wird exakt aufgezeigt, wie Sie den Wert Ihres Wohnobjektes verbessern können. Für Mehr Informationen kontaktieren Sie uns unter: bigdataaalen@gmail.com\n\n"\
             '✅ Zum Starten der Prognose bitte Parameter an der Sidebar auswählen und den "Parameter bestätigen" Button drücken.')
